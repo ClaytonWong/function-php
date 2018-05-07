a84ad820dc5aedae1bae6e748c44cc78a55aa96c
ClaytonWong <c_w_serious-github@yahoo.com.au>
2018-05-07 21:16:29 +1000

    Add file.
    Add function.php

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+  // Declare a function
+  function square($input) {
+    return $input * $input;
+  }
+
+  $input = 2;
+  $sqr = square($input); // Call square function
+
+  echo "Square of $input is $sqr";
+?>
